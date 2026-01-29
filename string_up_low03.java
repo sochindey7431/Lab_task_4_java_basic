4fedd49c7a4cf867bec04122816597428cbed2da
sochindey7431 <sochindey7431@gmail.com>
2026-01-29 09:31:51 +0600

    Forgive one code

--- string_up_low03.java
+++ string_up_low03.java
@@ -1,6 +1,7 @@
 public class string_up_low03 {
-      public static void main(String[] args) {
-    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    System.out.println("The length of the txt string is: " + txt.length());
+  public static void main(String[] args) {
+    String txt = "Hello World";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
   }
 }
